dd67e5a7a203e01870373a6d77109bb47704ac21
muhammadqasimjanjua162 <muhammadqasimjanjua162@gmail.com>
2025-02-17 02:19:38 +0500

    absolute added

--- src/components/HomePage/FeaturedListing.tsx
+++ src/components/HomePage/FeaturedListing.tsx
@@ -1,26 +1,37 @@
 import React from "react";
+import PropertyDisplay from "../PropertyDisplay";
 
 const FeaturedListing = () => {
   return (
-    <div className="grid grid-cols-3 gap-2 bg-dodgerblue p-2">
-      {/* <div className="col-span-2 bg-gray-200 text-center p-2 text-2xl">1</div> */}
-      <div className="bg-gray-200 text-center p-2 text-2xl">
-        <div className="flex  items-center space-x-2 ml-[11%]">
+    <div className="bg-dodgerblue p-2 items-start">
+      <div className=" text-center p-2 text-2xl bg-green-300">
+        <div className="flex items-center space-x-2 ml-[11%]">
           <span>01</span>
           <hr className="border-t-4 w-[30px] border-red-600" />
         </div>
         <span className="text-5xl font-bold uppercase">featured</span>
-        <div className="flex  items-center justify-between">
+        <div className="flex items-center justify-between">
           <hr className="border-t-4 w-[70%] border-gray-300" />
           <span>Listings</span>
         </div>
+
+        <button className="px-8 py-2 border border-gray-400 text-gray-800 uppercase relative">
+          <div className="w-[40px] h-[1px] bg-gray-300 absolute top-[50%] left-[-8%]"></div>
+
+          <a href="" className="uppercase text-xs font-semibold">
+            view all properties
+          </a>
+        </button>
+        <div className="absolute left-[50%] bottom-35 transform -translate-y-1/2 w-[40px] h-[1px] bg-gray-100"></div>
       </div>
-      <div className="bg-gray-200 text-center p-2 text-2xl">3</div>
-      <div className="bg-gray-200 text-center p-2 text-2xl">4</div>
-      <div className="bg-gray-200 text-center p-2 text-2xl">5</div>
-      <div className="bg-gray-200 text-center p-2 text-2xl">6</div>
-      <div className="bg-gray-200 text-center p-2 text-2xl">7</div>
-      <div className="bg-gray-200 text-center p-2 text-2xl">8</div>
+
+      <div className=" text-center p-2 text-2xl h-auto row-span-2 bg-red-500">
+        2
+      </div>
+      <div className="bg-yellow-400 text-center p-2 text-2xl h-auto row-span-2">
+        4
+      </div>
+     
     </div>
   );
 };
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,14 +8,67 @@ import FeaturedListing from "../components/HomePage/FeaturedListing";
 
 function HomePage() {
   return (
-    <div className="relative">
-      {/* <Header /> */}
-      <Hero />
-      <FeaturedListing />
+    <div className="grid grid-cols-12 gap-2">
+      {/* Hero Section */}
+      <div className="bg-gray-200 col-span-12 row-span-4 relative">
+        <Hero />
+      </div>
 
-      {/* <SomeHomeSection /> */}
-      <Footer />
+      {/* Featured Listing Section */}
+      <div className="text-center p-2 text-2xl bg-green-300 col-span-4 row-span-2">
+        <div className="flex items-center space-x-2 ml-[11%]">
+          <span>01</span>
+          <hr className="border-t-4 w-[30px] border-red-600" />
+        </div>
+        <span className="text-5xl font-bold uppercase">featured</span>
+        <div className="flex items-center justify-between">
+          <hr className="border-t-4 w-[70%] border-gray-300" />
+          <span>Listings</span>
+        </div>
+
+        <button className="px-8 py-2 border border-gray-400 text-gray-800 uppercase relative">
+          <div className="w-[40px] h-[1px] bg-gray-300 absolute top-[50%] left-[-8%]"></div>
+
+          <a href="" className="uppercase text-xs font-semibold">
+            view all properties
+          </a>
+        </button>
+        <div className="absolute left-[50%] bottom-35 transform -translate-y-1/2 w-[40px] h-[1px] bg-gray-100"></div>
+      </div>
+
+      {/* Another Section */}
+      <div className="text-center p-2 text-2xl h-auto bg-red-500 col-span-4 row-span-2">
+        2
+      </div>
+
+      {/* Lorem Ipsum Section */}
+      <div className="bg-yellow-400 text-center p-2 text-2xl  left-[67%] top-[60%] row-start-5 absolute  col-span-4 row-span-2  z-10">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        accusamus aperiam eos ea nemo natus quidem voluptate praesentium nobis,
+        qui vero a aliquid facere incidunt expedita adipisci aut vel,
+        consequuntur dignissimos neque possimus velit quasi dolorum assumenda?
+        Deserunt adipisci fugit corrupti minima inventore quibusdam iste est
+      </div>
+
+      {/* Footer */}
+      <div className="bg-gray-200 col-span-12">
+        <Footer />
+      </div>
     </div>
+
+    /* <div className="grid grid-rows-[150px_1fr] grid-cols-[250px_1fr] bg-cyan-400 w-[80vw] mx-[10vw] my-[8vw] h-[200px]">
+      <h1 className="text-black font-bold">Element 1</h1>
+
+      <div className="opacity-80 h-[150px] w-[190px] bg-yellow-400 row-start-1 col-start-1 col-end-2 row-end-2 z-10">
+        <h2 className="text-gray-700 font-bold py-[25px] px-[15px] mb-[15px]">Element 1.1</h2>
+        <p className="px-[10px] py-[5px]">Stacked using CSS Grid</p>
+      </div>
+
+      <div className="opacity-80 h-[150px] w-[190px] bg-yellow-400 row-start-1 col-start-1 col-end-2 row-end-2 z-0">
+        <h2 className="text-gray-700 font-bold py-[25px] px-[15px] mb-[15px]">Element 1.2</h2>
+        <p className="px-[10px] py-[5px]">Stacked using CSS Grid</p>
+      </div>
+    </div> */
   );
 }
 
